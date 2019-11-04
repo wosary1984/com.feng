1741138bcba23bfdd2c353b10d3aa86d95f05efe
bruce wang <bruce.wang01@sap.com>
2019-11-04 18:25:02 +0800

    add new project

---
+++ com.feng.security/src/main/java/com/feng/MainApplication.java
@@ -0,0 +1,23 @@
+
+package com.feng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Profile;
+
+@Profile("security")
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainApplication.class);
+	}
+
+}
